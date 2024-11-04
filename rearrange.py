21dfa5ee3fd1721c81ce2abf8105088296a10085
MohammadA20 <mohamadadada123@gmail.com>
2024-11-04 16:15:52 +0500

    Added a function called special_arrangement take an argument nums

--- rearrange.py
+++ rearrange.py
@@ -2,21 +2,24 @@
 In this program, we will list the even numbers before the odd numbers
 """
 
-LIST_NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-print(LIST_NUMBERS)
+LIST_NUMBERS = []
 
 EVEN_NUMBERS = []
 ODD_NUMBERS = []
 
+def special_rearrangement(nums):
 # In this for loop, we will check if the number is even or odd
-for sort in sorted(LIST_NUMBERS):
-    if sort % 2 == 0:
-        EVEN_NUMBERS.append(sort)
-        print(sort, " is an even number")
-    else:
-        ODD_NUMBERS.append(sort)
-        print(sort, " is an odd number")
+    nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+    print(nums)
+    for sort in sorted(nums):
+        if sort % 2 == 0:
+            print(sort, " is an even number")
+            EVEN_NUMBERS.append(sort)
+        else:
+            print(sort, " is an odd number")
+            ODD_NUMBERS.append(sort)
 
+special_rearrangement(LIST_NUMBERS)
 # Then  we will add the numbers sorted from even to odd numbers in the list
 NUMBERS_COMBINED = EVEN_NUMBERS + ODD_NUMBERS
 print("This is the list with an even numbers before odd numbers:", NUMBERS_COMBINED)
